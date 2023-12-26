f83ec48753ca83486c0f81de0a78f7ef692c4575
Adetayo <adetayo.akinsanya@prophius.com>
2023-12-26 12:14:53 +0100

    Minor fix

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,6 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import RalipoLogo from '/logo.svg';
-import './App.css';
 import { RalipoProvider } from '.';
+import './App.css';
+import RalipoLogo from '/logo.svg';
 
 function App() {
   return (
